689c526cc0635c54c984a2ac57a0c6e4c4a11479
david haioum <dav6983@gmail.com>
2020-05-01 16:43:02 +0800

    renamed certain functions

--- src/main/java/interestcalculator/InterestCalculator.java
+++ src/main/java/interestcalculator/InterestCalculator.java
@@ -3,21 +3,21 @@ package interestcalculator;
 import java.util.Scanner;
 
 public class InterestCalculator {
-    private static void calculateInterestSBaccount(Scanner sc) {
+    private static void enterAndComputeSBamount(Scanner sc) {
         SBaccount sb = new SBaccount();
         System.out.println("Enter the Average SB amount ");
         double amount = sc.nextDouble();
         System.out.println("Interest gained is : $ " + sb.calculateInterest(amount));
     }
 
-    private static void calculateInterestFDaccount(Scanner sc) {
+    private static void enterAndComputeFDaccount(Scanner sc) {
         FDaccount fd = new FDaccount();
         System.out.println("Enter the FD Amount");
         double fAmount = sc.nextDouble();
         System.out.println("Interest gained is: $ " + fd.calculateInterest(fAmount));
     }
 
-    private static void calculateInterestRDaccount(Scanner sc) {
+    private static void enterAndComputeRDaccount(Scanner sc) {
         RDaccount rd = new RDaccount();
         System.out.println("Enter the RD amount");
         double rAmount = sc.nextDouble();
@@ -33,13 +33,13 @@ public class InterestCalculator {
             int choice = sc.nextInt();
             switch (choice) {
                 case 1:
-                    calculateInterestSBaccount(sc);
+                    enterAndComputeSBamount(sc);
                     break;
                 case 2:
-                    calculateInterestFDaccount(sc);
+                    enterAndComputeFDaccount(sc);
                     break;
                 case 3:
-                    calculateInterestRDaccount(sc);
+                    enterAndComputeRDaccount(sc);
                     break;
                 case 4:
                     System.out.println("DO YOU WANT TO CALCULATE AGAIN ????" + " "
